dbf0d10cfc7a19458af82259a9359a9019670d05
Ibrahim Hany <ibrahim.hany96@hotmail.com>
2022-03-15 10:26:37 +0200

    second edit

--- src/main/java/com/mahmoud/springboot/v1/MobileApplication.java
+++ src/main/java/com/mahmoud/springboot/v1/MobileApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MobileApplication {
-    //hema edit
+    //hema edit222
     public static void main(String[] args) {
         SpringApplication.run(MobileApplication.class, args);
     }
